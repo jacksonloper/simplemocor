e7afc6e2ee69d64bca490e66b535e950ba1c4257
Jackson Loper <hloper@gmail.com>
2019-09-26 15:51:23 -0400

    Improvements!

--- __init__.py
+++ __init__.py
@@ -3,6 +3,130 @@ import numpy as np
 import scipy as sp
 import scipy.signal
 import scipy.ndimage
+import types  
+import multiprocessing    
+import multiprocessing.pool
+
+class MocorOptimizer:
+    def __init__(self,guys,clipin,tqdmnotebook=False,nthreads=8):
+        self.guys=guys
+        self.clipin=clipin
+        self.tqdmnotebook=tqdmnotebook
+        self.nthreads=nthreads
+
+    def initialize(self):
+        '''
+        Initializes a reasonable guess for the motion correction of guys
+        '''
+        self.status='initializing'
+        sizes=np.array([x.shape for x in self.guys])
+        shp=np.min(sizes,axis=0)
+        sl=(slice(0,shp[0]),slice(0,shp[1]))
+        self.template = np.mean([x[sl] for x in self.guys],axis=0)
+        self._get_offsets()
+
+    def _get_offsets(self):
+        binsize=50
+        if self.tqdmnotebook:
+            import tqdm
+            r=lambda x: tqdm.tqdm_notebook(x,total=len(self.guys)//binsize)
+        else:
+            r=lambda x:x
+
+        offsets=np.zeros((len(self.guys),2),dtype=np.int)
+        for i,v in r(get_offsets_multithreaded_iterator(
+                        self.guys,
+                        self.template,
+                        self.clipin,
+                        nthreads=self.nthreads,
+                        binsize=binsize)):
+            offsets[i]=v
+        self.offsets=offsets
+
+    def update(self):
+        '''
+        Performs a round of optimization to improve the template and offsets
+        '''
+        self.keepgoing=True
+
+        # perform motion correction based on current offset
+        tseries2,temppos=apply_a_mocor(self.guys,self.offsets)
+
+        # get a new template out of that, set that to our new template
+        self.template = np.mean(tseries2,axis=0)
+
+        # compute the new offsets for that template
+        self._get_offsets()
+
+def get_offsets_multithreaded_iterator(guys,template,clipin,nthreads=8,binsize=50):
+    '''
+    Find a location for the template in each guy, sliding the template around by at most 'clipin'
+    '''
+
+    n=len(guys)
+    clipin=int(clipin)
+    temp=template[clipin:-clipin,clipin:-clipin]
+    def go(sl):
+        lst=[]
+        for i in sl:
+            img=guys[i]
+            assert (np.array(temp.shape) < np.array(img.shape)).all()
+            rez=normcor(img,temp)
+            lst.append(np.unravel_index(np.argmax(rez),rez.shape))
+        return sl,np.array(lst)-clipin
+
+    bins=np.r_[0:len(guys):binsize,len(guys)]
+    todo=[range(bins[i],bins[i+1]) for i in range(len(bins)-1)]
+    with multiprocessing.pool.ThreadPool(nthreads) as ex:
+        yield from ex.imap_unordered(go,todo)  
+
+def apply_a_mocor(guys,gamma):
+    '''
+    Input:
+    - guys are a list of images, possibly with different sizes
+    - gamma indicates the upper-left corner of where a reference template should be placed in each image
+
+    Output:
+    - tseries2 - A regular Nt x Nrow x Ncol array
+    - temppos  - the canonical location of the reference template for each t
+
+    In the resulting tseries2, we should have that
+
+       tseries2[t,temppos[0],temppos[1]] 
+
+    corresponds to the upper-left corner of the reference template
+    for every t.  
+
+    '''
+
+    temppos=np.min(gamma,axis=0) 
+    sizes=np.array([x.shape for x in guys])
+
+    # tseries[t,0,0] = guys[t,startsites[t][0],startsites[t][1]]
+    startsites=gamma-temppos
+
+    # need 
+    # tseries[t,Nrows-1,Ncols-1] = guys[t,startsites[t][0]+Nrows-1,startsites[t][1]+Ncols-1]
+    # so need startsites+[Nrows,Ncols] <= sizes
+    # so need [Nrows,Ncols] <= sizes - startsites
+    targsh = np.min(sizes - startsites,axis=0)
+
+    # do the mocor
+    n=len(guys)
+    tseries2=np.zeros((n,targsh[0],targsh[1]),dtype=guys[0].dtype)
+
+    for i in range(n):
+        tseries2[i]=guys[i][
+            startsites[i,0]:startsites[i,0]+targsh[0],
+            startsites[i,1]:startsites[i,1]+targsh[1]]
+             
+    return tseries2,temppos
+
+
+###################################################
+###################################################
+###################################################
+###################################################
 
 def unif_filt1_valid(A,m):
     assert m>1
@@ -153,91 +277,4 @@ def sliceit(F,st1,st2,en2,fill_value=0.0):
             raise Exception("Need a fill value for %s %s %s %s"%(st1,en1,st2,en2))
         else:
             return np.ones(gsize)*fill_value
-             
-def calc_mocor(guys,temp,clipin=20,verbose=True):
-    '''
-    Find best position for template for each guy in guys.
-
-    Returns indices so that if we imagine guys[t] takes up
-    the rectangle from (0,0) to guys[t].shape, then
-    the upper-left corner of template should be placed at Gamma[t]
-    to make it line up with guys[t].
-    '''
-    n=len(guys)
-    temp=temp[clipin:-clipin,clipin:-clipin]
-    Gamma=np.zeros((n,2),dtype=np.int)
-    for i in tqdm_dispatcher(n,verbose=verbose):
-        img=guys[i]
-        assert (np.array(temp.shape) < np.array(img.shape)).all()
-        rez=normcor(img,temp)
-        Gamma[i]=np.unravel_index(np.argmax(rez),rez.shape)
-    return Gamma-clipin
-
-def do_mocor(guys,gamma,verbose=True):
-    '''
-    Input:
-    - guys are a list of images, possibly with different sizes
-    - gamma indicates the upper-left corner of where a reference template should be placed in each image
-
-    Output:
-    - tseries2 - A regular Nt x Nrow x Ncol array
-    - temppos  - the canonical location of the reference template for each t
-
-    In the resulting tseries2, we should have that
-
-       tseries2[t,temppos[0],temppos[1]] 
-
-    corresponds to the upper-left corner of the reference template
-    for every t.  
-
-    '''
-
-    temppos=np.min(gamma,axis=0)
-    sizes=np.array([x.shape for x in guys])
-
-    # tseries[t,0,0] = guys[t,startsites[t][0],startsites[t][1]]
-    startsites=gamma-temppos
-
-    # need 
-    # tseries[t,Nrows-1,Ncols-1] = guys[t,startsites[t][0]+Nrows-1,startsites[t][1]+Ncols-1]
-    # so need startsites+[Nrows,Ncols] <= sizes
-    # so need [Nrows,Ncols] <= sizes - startsites
-    targsh = np.min(sizes - startsites,axis=0)
-
-    # do the mocor
-    n=len(guys)
-    tseries2=np.zeros((n,targsh[0],targsh[1]),dtype=guys[0].dtype)
-
-
-    for i in tqdm_dispatcher(n,verbose=verbose):
-        tseries2[i]=guys[i][
-            startsites[i,0]:startsites[i,0]+targsh[0],
-            startsites[i,1]:startsites[i,1]+targsh[1]]
-             
-    return tseries2,temppos
-
-def find_central_template_multiround(guys,clipin=20,rounds=3,verbose=True):
-    '''
-    Attempts to find a large reference template which can be found (approximately)
-    somewhere in every image.
-    '''
-
-    # get shitty first pass template
-    sizes=np.array([x.shape for x in guys])
-    shp=np.min(sizes,axis=0)
-    sl=(slice(0,shp[0]),slice(0,shp[1]))
-    template = np.mean([x[sl] for x in guys],axis=0)
-
-    for i in tqdm_dispatcher(rounds,verbose=verbose):  
-        # find that template in guys  
-        gamma=calc_mocor(guys,template,clipin=clipin,verbose=False)
-
-        # construct a large-as-possible regular array
-        # where the template can be found in a common
-        # relative position in each frame
-        tseries2,temppos=do_mocor(guys,gamma,verbose=False)
-
-        # use that to make a new template
-        template = np.mean(tseries2,axis=0)
-
-    return template
+   
